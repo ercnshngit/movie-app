0b6a5c7fe2c840eb97a004fea97af274a848891e
Ercan Şahin <ercansahin@outlook.com>
2024-05-19 13:10:53 +0300

    feat: added type listing in homepage

--- src/routes/homepage/index.tsx
+++ src/routes/homepage/index.tsx
@@ -1,3 +1,16 @@
+import TypeList from "@/components/type/type-list";
+import { Breadcrumb } from "@/components/ui/breadcrumb";
+import { TYPE_LIST } from "@/mock/types";
+
 export default function Homepage() {
-  return <div></div>;
+  return (
+    <>
+      <Breadcrumb>
+        <h2 className="text-lg">Popüler Başlıklar</h2>
+      </Breadcrumb>
+      <div className="container mx-auto py-10">
+        <TypeList types={TYPE_LIST} />
+      </div>
+    </>
+  );
 }
