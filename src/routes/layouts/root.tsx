19b3e6d6e39b0d86b670e472d7bde53bf26b9fd0
Ercan Şahin <ercansahin@outlook.com>
2024-05-19 14:27:54 +0300

    feat: Added loading screen and fake delay for presentation purposes

--- src/routes/layouts/root.tsx
+++ src/routes/layouts/root.tsx
@@ -1,12 +1,23 @@
 import Header from "@/components/header";
+import { cn } from "@/lib/utils";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 export default function RootLayout() {
+  // navigation hook is used to get the current navigation state (e.g. form submit, data loading, etc.)
+  const navigation = useNavigation();
+
   return (
     <div>
       <Header />
-      <Outlet />
+      <main
+        className={cn(
+          "transition-opacity duration-300 ease-in-out",
+          navigation.state === "loading" && "opacity-50 pointer-events-none"
+        )}
+      >
+        <Outlet />
+      </main>
     </div>
   );
 }
--- src/services/api/movies/index.ts
+++ src/services/api/movies/index.ts
@@ -1,3 +1,4 @@
+import { fakeDelay } from "@/lib/utils";
 import { MOVIES } from "@/mock/movies";
 import { MoviesResponse } from "@/types/movies";
 
@@ -7,10 +8,12 @@ export function getMovies() {
   return data;
 }
 
-export function getMovieByType(type: string) {
+export async function getMovieByType(type: string) {
   const data = MOVIES.entries.filter((movie) => {
     return movie.programType === type;
   });
+
+  await fakeDelay(import.meta.env.VITE_MOCK_API_FAKE_DELAY_IN_MS || 0);
 
   return data;
 }
