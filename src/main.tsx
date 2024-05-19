e55ba2d11693d9a4ed9708cda3f5850061011d13
Ercan Şahin <ercansahin@outlook.com>
2024-05-19 09:55:34 +0300

    feat(routes): listing page added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Homepage from "./routes/homepage";
+import ListingPage from "./routes/list-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
+  },
+  {
+    path: "/list/:type",
+    element: <ListingPage />,
   },
 ]);
 
