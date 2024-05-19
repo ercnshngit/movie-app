c159373e3399e5785e8e5ed7d86b429448976c4a
Ercan Şahin <ercansahin@outlook.com>
2024-05-19 13:10:30 +0300

    feat: Added no content page

--- src/components/movie/movie-list.tsx
+++ src/components/movie/movie-list.tsx
@@ -3,7 +3,12 @@ import MovieCard from "./movie-card";
 
 export default function MovieList({ movies }: { movies: Movie[] }) {
   return (
-    <div className="grid grid-cols-4 gap-10">
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-10">
+      {movies.length === 0 && (
+        <div className="col-span-4 text-center text-lg">
+          Bu tipte içerik bulunamadı.
+        </div>
+      )}
       {movies.map((movie) => (
         <div key={movie.title} className="h-[400px] w-full overflow-hidden">
           <MovieCard movie={movie} />
