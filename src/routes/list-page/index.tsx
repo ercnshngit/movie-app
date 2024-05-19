276416042fe16c1b5719c18e31270bfdcdcb556e
Ercan Şahin <ercansahin@outlook.com>
2024-05-19 11:09:19 +0300

    added list page type 404

--- src/routes/list-page/index.tsx
+++ src/routes/list-page/index.tsx
@@ -1,3 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Link, useParams } from "react-router-dom";
+
 export default function ListingPage() {
+  let params = useParams();
+  let type = params.type || null;
+
+  if (!type) {
+    return (
+      <div>
+        Aradığınız başlık bulunamamıştır.{" "}
+        <Link to="/">
+          <Button asChild>Anasayfaya dön</Button>
+        </Link>
+      </div>
+    );
+  }
+
   return <div></div>;
 }
